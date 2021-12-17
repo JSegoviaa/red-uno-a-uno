94ff366b77e5f163ee54947b4c3bb24ddd29e926
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-17 11:53:52 -0500

    button default

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -3,16 +3,22 @@ type Btn = "Primary" | "Secondary" | "Green" | "Add";
 interface Props {
   titulo: string;
   onClick?: () => void;
-  btn: Btn;
+  btn?: Btn;
 }
 
 const Button = ({ titulo, onClick, btn = "Primary" }: Props) => {
   return (
     <>
-      {btn === "Primary" ? <div>BTN primario {titulo} </div> : null}
-      {btn === "Secondary" ? <div>BTN secundario {titulo} </div> : null}
-      {btn === "Green" ? <div>BTN verde {titulo} </div> : null}
-      {btn === "Add" ? <div>Agregar {titulo} </div> : null}
+      {btn === "Primary" ? (
+        <div onClick={onClick}>BTN primario {titulo} </div>
+      ) : null}
+      {btn === "Secondary" ? (
+        <div onClick={onClick}>BTN secundario {titulo} </div>
+      ) : null}
+      {btn === "Green" ? (
+        <div onClick={onClick}>BTN verde {titulo} </div>
+      ) : null}
+      {btn === "Add" ? <div onClick={onClick}>Agregar {titulo} </div> : null}
     </>
   );
 };
