ae64e5d8797a7d2084570a1bea9f917de7082626
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-17 11:55:26 -0500

    botones

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -9,16 +9,10 @@ interface Props {
 const Button = ({ titulo, onClick, btn = "Primary" }: Props) => {
   return (
     <>
-      {btn === "Primary" ? (
-        <div onClick={onClick}>BTN primario {titulo} </div>
-      ) : null}
-      {btn === "Secondary" ? (
-        <div onClick={onClick}>BTN secundario {titulo} </div>
-      ) : null}
-      {btn === "Green" ? (
-        <div onClick={onClick}>BTN verde {titulo} </div>
-      ) : null}
-      {btn === "Add" ? <div onClick={onClick}>Agregar {titulo} </div> : null}
+      {btn === "Primary" ? <div onClick={onClick}>{titulo} </div> : null}
+      {btn === "Secondary" ? <div onClick={onClick}> {titulo} </div> : null}
+      {btn === "Green" ? <div onClick={onClick}>{titulo} </div> : null}
+      {btn === "Add" ? <div onClick={onClick}> {titulo} </div> : null}
     </>
   );
 };
