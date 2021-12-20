c3fef63d9dc806ff9dde28e552a129da706a3f66
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-20 11:49:23 -0500

    perfil

--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import Perfil from "../../components/paginas/perfil/perfil/Perfil";
 import SEO from "../../components/seo/SEO";
