cb9d91ee73a6d1b3385db74c89a9a81468cd1e96
JSegovia <acosta.segoviaa@gmamil.com>
2021-12-20 17:55:51 -0500

    detalles propiedad

--- src/pages/propiedades/detalles-de-propiedad/[id].tsx
+++ src/pages/propiedades/detalles-de-propiedad/[id].tsx
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
+import Contact from "../../../components/paginas/propiedades/detalles/Contact";
 import Detalles from "../../../components/paginas/propiedades/detalles/Detalles";
 import Slider from "../../../components/paginas/propiedades/detalles/Slider";
 import SEO from "../../../components/seo/SEO";
-import Contacto from "../../contacto";
 
 const DetallesPropiedad = () => {
   const { asPath } = useRouter();
@@ -12,7 +12,7 @@ const DetallesPropiedad = () => {
       <SEO titulo="Detalles de la propiedad" url={asPath} />
       <Slider />
       <Detalles />
-      <Contacto />
+      <Contact />
     </>
   );
 };
