8d891a9af40f32370981f01e1afcee98dcf726c5
JSegovia <acosta.segoviaa@hotmail.com>
2021-12-20 22:05:05 -0500

    useForm

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent, useState } from 'react';
+
+export const useForm = <T extends Object>(initialState: T) => {
+  const [formulario, setFormulario] = useState(initialState);
+
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+
+    setFormulario({ ...formulario, [name]: value });
+  };
+
+  return { formulario, handleChange };
+};
