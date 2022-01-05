fae8369ced45a58f42aa872c98e2ce3fad1eedb5
alvaro <alsato.650cc@gmail.com>
2022-01-04 19:02:39 -0500

    asdasd

--- src/components/ui/purpleheader/PurpleHeader.tsx
+++ src/components/ui/purpleheader/PurpleHeader.tsx
@@ -1,29 +1,38 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { AuthContext } from '../../../context/auth/AuthContext'
 import styles from './PurpleHeader.module.css'
 
 const PurpleHeader = () => {
+    const {auth}=useContext(AuthContext)
     return (
         <div className={styles.purpleNav2}>
             <ul className="nav d-flex justify-content-center">
-                <li className="nav-item mt-2">
-                    <Link href="/perfil/mis-propiedades"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mis Propiedades</div></Link>
-                </li>
-                <li className="nav-item mt-2">
-                    <Link href="/perfil/mis-favoritos"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mis Favoritos</div></Link>
-                </li>
-                <li className="nav-item mt-2">
-                    <Link href="/perfil"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mi Cuenta</div></Link>
-                </li>
-                <li className="nav-item mt-2">
-                    <Link href="/perfil/historial-de-inmueble"><div className={`${styles.purpleLinks} mx-3 pointer`}>Historial de Inmuebles</div></Link>
-                </li>
+                { auth.uid?
+                    <>
+                        <li className="nav-item mt-2">
+                            <Link href="/perfil/mis-propiedades"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mis Propiedades</div></Link>
+                        </li>
+                        <li className="nav-item mt-2">
+                            <Link href="/perfil/mis-favoritos"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mis Favoritos</div></Link>
+                        </li>
+                        <li className="nav-item mt-2">
+                            <Link href="/perfil"><div className={`${styles.purpleLinks} mx-3 pointer`}>Mi Cuenta</div></Link>
+                        </li>
+                        <li className="nav-item mt-2">
+                            <Link href="/perfil/historial-de-inmueble"><div className={`${styles.purpleLinks} mx-3 pointer`}>Historial de Inmuebles</div></Link>
+                        </li>
+                    </>
+                :null}
                 <li className="nav-item">
                     <input type="text" className={styles.searchInput} placeholder="Busca aquí..." />
                     <button className={styles.searchBtn}><i className="bi bi-search" /></button>
                 </li>
-                <li className="nav-item">
-                    <img className={`${styles.notificacion} mx-3 pointer`} src="/images/icons/Notificaciones.png" alt="..." />
-                </li>
+                { auth.uid?
+                    <li className="nav-item">
+                        <img className={`${styles.notificacion} mx-3 pointer`} src="/images/icons/Notificaciones.png" alt="..." />
+                    </li>
+                :null}
             </ul>
         </div>
     )
