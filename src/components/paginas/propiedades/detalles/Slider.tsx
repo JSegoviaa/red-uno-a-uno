494abb9711cb034d950309de80825f6be3b4aded
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-06 11:21:50 -0500

    slider

--- src/components/paginas/propiedades/detalles/Slider.tsx
+++ src/components/paginas/propiedades/detalles/Slider.tsx
@@ -1,8 +1,29 @@
-import styles from "./Inmueble.module.css"
+import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "./Inmueble.module.css";
+import "swiper/css";
+
+const imagenes = [
+  { id: 1, img: "" },
+  { id: 2, img: "" },
+  { id: 3, img: "" },
+  { id: 4, img: "" },
+  { id: 5, img: "" },
+  { id: 6, img: "" },
+  { id: 7, img: "" },
+];
+
 const Slider = () => {
-  return <div className="text-center">
-    <div className={styles.sliderEjemplo}></div>
-  </div>;
+  return (
+    <div className="text-center">
+      <Swiper className="mySwiper">
+        {imagenes.map((image) => (
+          <SwiperSlide key={image.id}>
+            <div className={styles.sliderEjemplo}>{image.id}</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
