16ecea3e133a3a6210fc8baf81dc0920563f7790
alvaro <alsato.650cc@gmail.com>
2022-01-07 13:17:42 -0500

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,20 @@
 const NotFound = () => {
-  return <div>No hay nada mierda</div>;
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-12 text-center">
+          <img className="img-center" src="/images/content/404.png" alt="mantenimiento" />
+        </div>
+        <div className="col-12 titulo my-2">
+          ¡Oops! Error 404
+        </div>
+        <div className="col-12 subtitulo">
+          Al parecer el lugar al que quieres acceder <br /> no existe <br /> :(
+            <br /><br />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
