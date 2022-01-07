101a65621fb86c3b2502f4420ee95d0567b8e4bf
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-07 17:05:32 -0500

    se agregan coordenadas de los inmuebles en el mapa principal

--- src/components/paginas/inicio/MapaBuscador.tsx
+++ src/components/paginas/inicio/MapaBuscador.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
-import { GoogleMap } from "@react-google-maps/api";
+import { memo, useContext } from "react";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 import { MapContext } from "../../../context/map/MapContext";
+import { useInmuebles } from "../../../hooks/useInmuebles";
 
 const containerStyle = {
   width: "100%",
@@ -9,14 +10,22 @@ const containerStyle = {
 
 const MapaUbicacion = () => {
   const { coordenadas } = useContext(MapContext);
+  const { inmuebles } = useInmuebles();
 
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={{ lat: coordenadas.lat, lng: coordenadas.lng }}
       zoom={14}
-    />
+    >
+      {inmuebles?.map((inmueble) => (
+        <Marker
+          position={{ lat: inmueble.lat, lng: inmueble.lng }}
+          icon="/images/icons/marcador-ubicacion.png"
+        />
+      ))}
+    </GoogleMap>
   );
 };
 
-export default MapaUbicacion;
+export default memo(MapaUbicacion);
