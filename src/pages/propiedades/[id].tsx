d01839fdddb4593f40601317cda011d7ab4020e5
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-11 13:29:39 -0500

    fallback false

--- src/pages/propiedades/[id].tsx
+++ src/pages/propiedades/[id].tsx
@@ -18,7 +18,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     return { params: { id: path.slug.toString() } };
   });
 
-  return { paths, fallback: true };
+  return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
