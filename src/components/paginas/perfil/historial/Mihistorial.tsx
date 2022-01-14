bae1090e1adee3bf9ff3b9054fe898a01bb10c87
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-14 12:58:38 -0500

    historial

--- src/components/paginas/perfil/historial/Mihistorial.tsx
+++ src/components/paginas/perfil/historial/Mihistorial.tsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 import { AuthContext } from "../../../../context/auth/AuthContext";
 import { useHistorial } from "../../../../hooks/useUserInfo";
 import Loading from "../../../ui/loading/Loading";
 import styles from "./Mihistorial.module.css";
 import { publicadoHace } from "../../../../helpers/horaMes";
 import { eliminarHist } from "../../../../helpers/fetch";
-import { toast } from "react-toastify";
 
 const Mihistorial = () => {
   const { auth } = useContext(AuthContext);
@@ -20,44 +20,54 @@ const Mihistorial = () => {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <br />
-          <table className={`${styles.customTable}`}>
-            {isLoading ? (
-              <Loading />
-            ) : (
-              <tbody>
-                {historial
-                  .map((hist, i) => (
-                    <tr key={hist._id} className={`${styles.thover}`}>
-                      <td className={styles.tNumber}>{i + 1} </td>
-                      <td
-                        onClick={() => goToProperty(hist.inmueble.slug)}
-                        className={`${styles.content} pointer`}
-                      >
-                        {hist.inmueble.titulo}
-                      </td>
-                      <td align="center">
-                        Visto {publicadoHace(hist.createdAt)}
-                      </td>
+    <section className={styles.section}>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <br />
+            <table className={`${styles.customTable}`}>
+              {isLoading ? (
+                <Loading />
+              ) : (
+                <>
+                  {historial.length === 0 ? (
+                    <div className={`${styles.titulo} text-center`}>
+                      No tienes búsquedas recientes
+                    </div>
+                  ) : (
+                    <tbody>
+                      {historial
+                        .map((hist, i) => (
+                          <tr key={hist._id} className={`${styles.thover}`}>
+                            <td className={styles.tNumber}>{i + 1} </td>
+                            <td
+                              onClick={() => goToProperty(hist.inmueble.slug)}
+                              className={`${styles.content} pointer`}
+                            >
+                              {hist.inmueble.titulo}
+                            </td>
+                            <td align="center">
+                              Visto {publicadoHace(hist.createdAt)}
+                            </td>
 
-                      <td align="center">
-                        <i
-                          onClick={() => handleDelete(hist._id)}
-                          className={`${styles.deleteI} bi bi-x-circle-fill pointer`}
-                        />
-                      </td>
-                    </tr>
-                  ))
-                  .reverse()}
-              </tbody>
-            )}
-          </table>
+                            <td align="center">
+                              <i
+                                onClick={() => handleDelete(hist._id)}
+                                className={`${styles.deleteI} bi bi-x-circle-fill pointer`}
+                              />
+                            </td>
+                          </tr>
+                        ))
+                        .reverse()}
+                    </tbody>
+                  )}
+                </>
+              )}
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
