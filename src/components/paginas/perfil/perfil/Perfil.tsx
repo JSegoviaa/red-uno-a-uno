4747e24e1ac93ed14de86e9e1ad32a6279f95787
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-17 16:57:41 -0500

    Se oculta input file

--- src/components/paginas/perfil/perfil/Perfil.tsx
+++ src/components/paginas/perfil/perfil/Perfil.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { AuthContext } from "../../../../context/auth/AuthContext";
@@ -9,9 +9,13 @@ const Perfil = () => {
   const router = useRouter();
   const { auth, fotoPerfil } = useContext(AuthContext);
   const [picture, setPicture] = useState("");
+  const [hover, setHover] = useState(false);
+  const inputFile = useRef<any>(null);
   const misPaquetes = () => router.push("/perfil/mis-paquetes");
   const misPropiedades = () => router.push("/perfil/mis-propiedades");
   const actualizarPerfil = () => router.push("/perfil/actualizar-perfil");
+
+  const abrirInputfile = () => inputFile.current.click();
 
   const handlePicture = async (e: any) => {
     e.preventDefault();
@@ -20,17 +24,43 @@ const Perfil = () => {
 
     await fotoPerfil(formData);
   };
+
+  const onMouseEnter = () => setHover(true);
+  const onMouseLeave = () => setHover(false);
+
   return (
     <Container>
       <div className="d-flex justify-content-center">
         <div className="text-center">
           <div className="pt-5 pb-3">
             <img
+              onClick={abrirInputfile}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+              className="pointer"
               src={auth.img}
               style={{ borderRadius: "50%" }}
               alt="Foto de perfil red1a1"
             />
+            {hover ? <div>Cambiar imagen de perfil</div> : null}
           </div>
+
+          <Form
+            onSubmit={handlePicture}
+            encType="multipart/form-data"
+            className="d-flex justify-content-center"
+          >
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="file"
+                ref={inputFile}
+                style={{ display: "none" }}
+                onChange={(e: any) => setPicture(e.target.files[0])}
+              />
+            </Form.Group>
+            <>{picture ? <Button titulo="Subir imagen" /> : null}</>
+          </Form>
+
           <div className={styles.nombre}>
             {auth.nombre} {auth.apellido}
           </div>
@@ -40,16 +70,8 @@ const Perfil = () => {
           <div className={styles.telefono}>{auth.telefonoPersonal}</div>
         </div>
       </div>
+
       <hr />
-      <Form onSubmit={handlePicture} encType="multipart/form-data">
-        <Form.Group className="mb-3">
-          <Form.Control
-            type="file"
-            onChange={(e: any) => setPicture(e.target.files[0])}
-          />
-        </Form.Group>
-        <button>enviar</button>
-      </Form>
 
       <div className="py-5">
         <Row className="d-flex justify-content-center text-center">
