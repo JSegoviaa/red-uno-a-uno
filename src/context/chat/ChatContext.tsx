30348685073660783d00edcec106468a3d9e4fc9
JSegovia <acosta.segoviaa@hotmail.com>
2022-01-23 19:23:25 -0500

    chat reducer

--- src/context/chat/ChatContext.tsx
+++ src/context/chat/ChatContext.tsx
@@ -1,21 +1,50 @@
-import { createContext, Dispatch, FC, SetStateAction, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  FC,
+  SetStateAction,
+  useReducer,
+  useState,
+} from 'react';
+import { chatReducer } from '../../reducers/chatReducer';
 
 interface ContextProps {
   abrirChat: boolean;
   setAbrirChat: Dispatch<SetStateAction<boolean>>;
   minimizarChat: boolean;
   setMinimizarChat: Dispatch<SetStateAction<boolean>>;
+  chatState: any;
+  dispatch: Dispatch<any>;
 }
 
 export const ChatContext = createContext({} as ContextProps);
 
+const initialState: any = {
+  uid: '',
+  chatActivo: null,
+  usuarios: [],
+  mensajes: [],
+};
+
 export const ChatProvider: FC = ({ children }) => {
   const [abrirChat, setAbrirChat] = useState(false);
   const [minimizarChat, setMinimizarChat] = useState(true);
+  const [chatState, dispatch] = useReducer(
+    chatReducer,
+    initialState,
+    undefined
+  );
 
   return (
     <ChatContext.Provider
-      value={{ abrirChat, setAbrirChat, minimizarChat, setMinimizarChat }}
+      value={{
+        abrirChat,
+        setAbrirChat,
+        minimizarChat,
+        setMinimizarChat,
+        chatState,
+        dispatch,
+      }}
     >
       {children}
     </ChatContext.Provider>
---
+++ src/reducers/chatReducer.tsx
@@ -0,0 +1,6 @@
+export const chatReducer = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
