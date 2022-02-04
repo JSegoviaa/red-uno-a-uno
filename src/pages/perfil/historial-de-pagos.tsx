fedea29fc057776ceceddfa75a77e45836fd0809
JSegovia <acosta.segoviaa@gmamil.com>
2022-02-04 17:59:32 -0500

    historial de pagos

--- src/pages/perfil/historial-de-pagos.tsx
+++ src/pages/perfil/historial-de-pagos.tsx
@@ -1,11 +1,16 @@
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import HistorialPagos from "../../components/paginas/perfil/historial/HistorialPagos";
 import SEO from "../../components/seo/SEO";
 import Titulo from "../../components/ui/titulo/Titulo";
+import { AuthContext } from "../../context/auth/AuthContext";
+import { useHistorialPagos } from "../../hooks/useUserInfo";
 
 const HistorialDePagos = () => {
   const router = useRouter();
-
+  const { auth } = useContext(AuthContext);
+  // const { total, cargando, historialPago } = useHistorialPagos(auth.uid);
+  // console.log(historialPago);
   return (
     <div>
       <SEO titulo="Historial de pagos" url={router.asPath} />
