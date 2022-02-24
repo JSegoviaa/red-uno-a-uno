ab0dacc8cecb5419a60b854a9d43cacd46c07f25
JSegovia <acosta.segoviaa@gmamil.com>
2022-02-24 12:57:22 -0500

    términos y condiciones y aviso de privacidad

--- src/pages/aviso-de-privacidad.tsx
+++ src/pages/aviso-de-privacidad.tsx
@@ -1,5 +1,18 @@
+import { useRouter } from "next/router";
+import { Container } from "react-bootstrap";
+import SEO from "components/seo/SEO";
+
 const NoP = () => {
-  return <div>Aviso de privacidad</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <SEO titulo="Aviso de privacidad" url={router.asPath} />
+      <Container>
+        <h1 className="text-center">Aviso de privacidad</h1>
+      </Container>
+    </>
+  );
 };
 
 export default NoP;
--- src/pages/terminos-y-condiciones.tsx
+++ src/pages/terminos-y-condiciones.tsx
@@ -1,5 +1,18 @@
+import { useRouter } from "next/router";
+import { Container } from "react-bootstrap";
+import SEO from "components/seo/SEO";
+
 const ToS = () => {
-  return <div>Términos y condiciones</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <SEO titulo="Términos y condiciones" url={router.asPath} />
+      <Container>
+        <h1 className="text-center">Términos y condiciones</h1>
+      </Container>
+    </>
+  );
 };
 
 export default ToS;
