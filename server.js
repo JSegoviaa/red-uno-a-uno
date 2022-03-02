4652139c66d1905700e3ef5bfad2760b20183cfc
JSegovia <acosta.segoviaa@gmamil.com>
2022-03-02 18:41:46 -0500

    asd

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // const { createServer } = require('http')
 // const next = require('next')
- 
+
 // const isDevMode = process.env.NODE_ENV !== 'production'
 // const port = process.env.PORT ? process.env.PORT : 3000
- 
+
 // const nextjsApp = next({ dev: isDevMode })
 // const nextjsRequestHandler = nextjsApp.getRequestHandler()
- 
+
 // nextjsApp
 //   .prepare()
 //   .then(() => {
@@ -23,31 +23,31 @@
 //     console.error(ex.stack)
 //     process.exit(1)
 //   })
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+// const { createServer } = require('http')
+// const { parse } = require('url')
+// const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-const port = process.env.PORT || 3000
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+// const port = process.env.PORT || 3000
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     // Be sure to pass `true` as the second argument to `url.parse`.
+//     // This tells it to parse the query portion of the URL.
+//     const parsedUrl = parse(req.url, true)
+//     const { pathname, query } = parsedUrl
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+//     if (pathname === '/a') {
+//       app.render(req, res, '/a', query)
+//     } else if (pathname === '/b') {
+//       app.render(req, res, '/b', query)
+//     } else {
+//       handle(req, res, parsedUrl)
+//     }
+//   }).listen(port, (err) => {
+//     if (err) throw err
+//     console.log(`> Ready on http://localhost:${port}`)
+//   })
+// })
