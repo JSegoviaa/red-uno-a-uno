bf31d506ba4fb85c59dc7ad1a89320141a1dbaab
JSegovia <acosta.segoviaa@gmamil.com>
2022-03-03 11:47:17 -0500

    se agregan meta tags twitter

--- src/components/seo/SEO.tsx
+++ src/components/seo/SEO.tsx
@@ -9,29 +9,43 @@ interface Props {
 
 const desc =
   "Red 1a1 Nace para dar solución a las necesidades del mercado inmobiliario. Asesores y clientes podrán agregar, buscar y compartir propiedades. Red 1a1 Nace para dar solución a las necesidades del mercado actual, la fórmula es simple. Crear un entorno, en el cual, asesores e inmobiliarias puedan llegar a un mutuo acuerdo de intercambio de propiedades y clientes para que todos ganen. Red 1a1 es la solución para asesores independientes e inmobiliarias que permite la comunicación y el intercambio de carteras de clientes / propiedades. A través de esta herramienta versátil podrás agregar, buscar y compartir propiedades, también podrás comunicarte a través de un sistema de chat con otros asesores e inmobiliarias de la comunidad a nivel nacional.";
-const logo = "https://i.imgur.com/45hp3Ed.png";
+const logo =
+  "https://res.cloudinary.com/du6f7alxg/image/upload/v1646325450/red1a1/red_amrywg.png";
 
-const SEO = ({ titulo, descripcion = desc, url, img = logo }: Props) => {
+const SEO = ({ titulo, descripcion = desc, url, img }: Props) => {
   return (
     <Head>
-      <title>{titulo ? `Red 1 a 1 | ${titulo}` : "Red 1 a 1"}</title>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>{titulo ? `Red 1 a 1 | ${titulo}` : "Red 1 a 1"}</title>
       <meta name="description" content={descripcion} />
       <meta name="keywords" content="Red uno a uno, Red 1 a 1" />
       <link
         rel="canonical"
         href={url ? `https://red1a1.com${url}` : "https://red1a1.com"}
       />
+      <link rel="shortcut icon" href="/red.png" type="image/x-icon" />
+
+      {/* <!-- Open Graph / Facebook --> */}
       <meta
         property="og:title"
         content={titulo ? `Red 1 a 1 | ${titulo}` : "Red 1 a 1"}
       />
       <meta property="og:description" content={descripcion} />
-      <meta property="og:image" content={img} />
+      <meta property="og:image" content={img ? img : logo} />
       <meta property="og:url" content={`https://red1a1.com${url}`} />
       <meta property="og:site_name" content={`Red 1 a 1 | ${titulo}`} />
-      <link rel="shortcut icon" href="/red.png" type="image/x-icon" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content={img ? img : logo} />
+      <meta property="twitter:url" content={`https://red1a1.com${url}`} />
+      <meta
+        property="twitter:title"
+        content={titulo ? `Red 1 a 1 | ${titulo}` : "Red 1 a 1"}
+      />
+      <meta property="twitter:description" content={descripcion} />
+      <meta property="twitter:image" content={img ? img : logo} />
+
       <script
         type="text/javascript"
         async
