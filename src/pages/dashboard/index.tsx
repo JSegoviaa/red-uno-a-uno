bb111f0fee172ffdf03e4c08135f8d59a5ab5983
alvaro <alsato.650cc@gmail.com>
2022-03-11 12:14:50 -0500

    card resume

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -14,26 +14,34 @@ const Index = () => {
         <section className="my-5">
           <div className="container">
             <div className="row ">
-              <div className="col-5">
-                <div className={styles.Rcard}>
+              <div className="col-sm-12 col-md-12 col-lg-6 col-xl-5 mb-3">
+                <div className={`${styles.Rcard} pointer`}>
+                <div className={`${styles.RcardTitle} text-end`}>
+                    <i className="bi bi-wallet2"></i> Wallet
+                  </div>
+                  <div className={styles.cardName}>
+                    <div className={styles.backText}>
+                      <i className="bi bi-wallet2"></i> Wallet
+                    </div>
+                  </div>
                   <div className="row">
                     <div className="col-12 mb-3">
                       <div className={styles.labelC}>
                         Ingresos de hoy
                       </div>
                       <div className={styles.ingreso}>
-                        $0.00
+                        $3,899.00
                       </div>
                     </div>
-                    <div className="col-6 mb-3">
+                    <div className="col-sm-12 col-md-6 col-lg-6 mb-3">
                       <div className={styles.labelC}>
                         Ingresos de la ultima semana
                       </div>
                       <div className={styles.ingreso2}>
-                        $5,125,065.00
+                        $125,065.00
                       </div>
                     </div>
-                    <div className="col-6 mb-3">
+                    <div className="col-sm-12 col-md-6 col-lg-6 mb-3">
                       <div className={styles.labelC}>
                         Ingresos del ultimo mes
                       </div>
@@ -41,15 +49,15 @@ const Index = () => {
                         $5,125,065.00
                       </div>
                     </div>
-                    <hr />
+                    <hr className="p-0" />
                     <div className="col-12">
                       <div className="row">
-                        <div className="col-6">
+                        <div className="col-sm-12 col-md-6 col-lg-6">
                           <div className={styles.labelC}>
                             Ingreso estimado del año
                           </div>
                         </div>
-                        <div className="col-6">
+                        <div className="col-sm-12 col-md-6 col-lg-6">
                           <div className={styles.ingreso3}>
                             $15,125,065.00
                           </div>
