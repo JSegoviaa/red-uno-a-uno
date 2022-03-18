b770820368f446573ff675d3381b99a53b02d1d8
JSegovia <acosta.segoviaa@gmamil.com>
2022-03-18 14:44:38 -0500

    responsive buscador

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { FC, useContext, useEffect } from "react";
 import { useRouter } from "next/router";
+import MediaQuery from "react-responsive";
 import { AuthContext } from "../../context/auth/AuthContext";
 import VentanaChat from "../paginas/perfil/chats/VentanaChat";
 import Footer from "../ui/footer/Footer";
 import Header from "../ui/header/Header";
 import PurpleHeader from "../ui/purpleheader/PurpleHeader";
+import BuscadorRes from "components/ui/buscador/BuscadorRes";
 
 const Layout: FC = ({ children }) => {
   const { verificaToken } = useContext(AuthContext);
@@ -22,8 +24,13 @@ const Layout: FC = ({ children }) => {
         <>{children}</>
       ) : (
         <>
-          <Header />
-          <PurpleHeader />
+          <MediaQuery minWidth={992}>
+            <Header />
+            <PurpleHeader />
+          </MediaQuery>
+          <MediaQuery maxWidth={991}>
+            <BuscadorRes />
+          </MediaQuery>
           {children}
           <VentanaChat />
           <Footer />
--- src/components/paginas/inicio/BuscarZona.tsx
+++ src/components/paginas/inicio/BuscarZona.tsx
@@ -1,8 +1,17 @@
+import { MapContext } from "context/map/MapContext";
+import { useContext } from "react";
 import styles from "./Inicio.module.css";
 
 const BuscarZona = () => {
+  const { filtros } = useContext(MapContext);
   return (
-    <div className={`${styles.buscarZona} pointer`}>Buscar en esta zona</div>
+    <div
+      className={`${
+        filtros ? styles.buscarZonaWithFilter : styles.buscarZona
+      } pointer text-center`}
+    >
+      Buscar en esta zona
+    </div>
   );
 };
 
