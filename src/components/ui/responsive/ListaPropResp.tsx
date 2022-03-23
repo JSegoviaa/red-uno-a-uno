7caf0fc6c7e5e5bcde479b3b65af20954f27d446
JSegovia <acosta.segoviaa@gmamil.com>
2022-03-23 18:03:19 -0500

    asdf

--- src/components/ui/responsive/ListaPropResp.tsx
+++ src/components/ui/responsive/ListaPropResp.tsx
@@ -1,103 +1,181 @@
+import { useState } from "react";
 import styles from "./ResposiveStyles.module.css";
 
 const ListaPropResp = () => {
-    return (
-        <div className={styles.listaRespActive}>
-            <div className={`${styles.SlideLine} pointer`}></div>
-            <div className="row g-0">
-                <div className="col-12">
-                    {/* <div className={`${styles.LRtitle1} mb-3`}>
+  const [mostrarLista, setMostrarLista] = useState(true);
+
+  const mostrarListaF = () => setMostrarLista(!mostrarLista);
+
+  return (
+    <div
+      className={
+        mostrarLista ? styles.listaRespActive : styles.listaRespInactive
+      }
+    >
+      <div
+        onClick={mostrarListaF}
+        className={`${styles.SlideLine} pointer`}
+      ></div>
+      <div className="row g-0">
+        <div className="col-12">
+          {/* <div className={`${styles.LRtitle1} mb-3`}>
                         Lista de Inmuebles
                     </div> */}
-                    <div className={`${styles.LRtitle2}`}>
-                        Se encontraron 37 resultados
-                    </div>
-                </div>
-            </div>
-            {/* empieza lista */}
-            <div className={styles.ListContainer}>
-                <div className="row">
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                    <div className="col-12 mb-4">
-                        <div className={`${styles.cardLista} p-4`}>
-                            <br /><br /><br /><br />
-                            asdasdads
-                            <br /><br /><br /><br />
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <div className={`${styles.LRtitle2}`}>
+            Se encontraron 37 resultados
+          </div>
         </div>
-    )
-}
+      </div>
+      {/* empieza lista */}
+      <div className={styles.ListContainer}>
+        <div className="row">
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className={`${styles.cardLista} p-4`}>
+              <br />
+              <br />
+              <br />
+              <br />
+              asdasdads
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ListaPropResp
+export default ListaPropResp;
