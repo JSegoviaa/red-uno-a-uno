8d1cd1b4d38804c0860fc84be690582022099758
JSegovia <acosta.segoviaa@gmamil.com>
2022-03-23 14:06:15 -0500

    notificaciones

--- src/components/ui/responsive/NotificacionResp.tsx
+++ src/components/ui/responsive/NotificacionResp.tsx
@@ -1,18 +1,112 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "context/auth/AuthContext";
 import { useSolicitudes } from "hooks/useSolicitudes";
 import styles from "./ResposiveStyles.module.css";
 import { useWindowWide } from "hooks/useWindowWide";
 import { useRouter } from "next/router";
+import { fetchAceptarRechazarSolicitud, hora } from "helpers";
+import { production } from "credentials/credentials";
+import { Solicitud } from "interfaces/SolicitudInteface";
+import { toast } from "react-toastify";
+import Loading from "../loading/Loading";
 
 const NotificacionResp = () => {
   const { auth } = useContext(AuthContext);
   const { push } = useRouter();
-  const [width, setWidth] = useState(0);
   const wide = useWindowWide(991);
   const { solicitudes, setSolicitudes, cargando } = useSolicitudes(auth.uid);
 
   if (wide) push("/");
+
+  const aprobarSolicitud = async (
+    id: string,
+    titulo: string,
+    img: string,
+    correo: string
+  ) => {
+    const aprobacion = {
+      estado: "Aprobado",
+    };
+
+    const res = await fetchAceptarRechazarSolicitud(
+      `solicitud/aceptar/${id}`,
+      aprobacion
+    );
+
+    const body = {
+      nombre: auth.nombre,
+      apellido: auth.apellido,
+      titulo,
+      img,
+      correo,
+    };
+
+    if (res.ok) {
+      await fetch(`${production}/correos/solicitud-aprobada`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const solicitudAprobada: Solicitud[] = solicitudes?.map((solicitud) => {
+        if (solicitud._id === id) {
+          return { ...solicitud, estado: "Aprobado" };
+        }
+        return solicitud;
+      });
+      setSolicitudes(solicitudAprobada);
+      //   setContador((prev) => prev - 1);
+      toast.success(res.msg);
+    }
+  };
+
+  const rechazarSolicitud = async (
+    id: string,
+    titulo: string,
+    img: string,
+    correo: string
+  ) => {
+    const rechazo = {
+      estado: "Rechazado",
+    };
+
+    const body = {
+      nombre: auth.nombre,
+      apellido: auth.apellido,
+      titulo,
+      img,
+      correo,
+    };
+
+    const res = await fetchAceptarRechazarSolicitud(
+      `solicitud/rechazar/${id}`,
+      rechazo
+    );
+
+    if (res.ok) {
+      await fetch(`${production}/correos/solicitud-rechazada`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const solicitudRechazada: Solicitud[] = solicitudes?.map((solicitud) => {
+        if (solicitud._id === id) {
+          return { ...solicitud, estado: "Rechazado" };
+        }
+        return solicitud;
+      });
+      //   setContador((prev) => prev - 1);
+      setSolicitudes(solicitudRechazada);
+
+      toast.success(res.msg);
+    }
+  };
+
+  console.log(solicitudes);
 
   return (
     <div className={`${styles.notifi}`}>
@@ -21,35 +115,168 @@ const NotificacionResp = () => {
           <div className="col-12">
             <div className={styles.notifiContent}>
               <table>
-                <tr>
-                  <td valign="top">
-                    <div className={styles.imgContainer}>
-                      <img
-                        src=""
-                        alt=""
+                {cargando ? (
+                  <Loading />
+                ) : (
+                  <>
+                    {solicitudes.length === 0 ? (
+                      <div
+                        className="text-center py-5"
                         style={{
-                          borderRadius: "50%",
-                          width: "100%",
+                          color: "#7A7A7A",
+                          fontWeight: "500",
+                          fontSize: 30,
                         }}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div className={styles.notifiText}>
-                      <b>Don Macario</b> quiere que le compartas{" "}
-                      <b>Casa en venta en CDMX Polanco 271</b>
-                    </div>
-                    <div className="btns mt-3">
-                      <span className={`${styles.aceptar} pointer`}>
-                        Aceptar
-                      </span>{" "}
-                      <span className={`${styles.rechazar} pointer`}>
-                        Rechazar
-                      </span>
-                    </div>
-                    <div className={styles.hra}>12:35pm</div>
-                  </td>
-                </tr>
+                      >
+                        Aún no tiene notificaciones
+                      </div>
+                    ) : (
+                      <>
+                        {solicitudes.map((solicitud) => (
+                          <tr key={solicitud._id}>
+                            {solicitud.estado === "Pendiente" ? (
+                              <>
+                                <td valign="top">
+                                  <div className={styles.imgContainer}>
+                                    <img
+                                      src={solicitud.usuario.img}
+                                      alt={solicitud.usuario.nombre}
+                                      style={{
+                                        borderRadius: "50%",
+                                        width: "100%",
+                                      }}
+                                    />
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className={styles.notifiText}>
+                                    <b>
+                                      {solicitud.usuario.nombre}{" "}
+                                      {solicitud.usuario.apellido}
+                                    </b>{" "}
+                                    quiere que le compartas{" "}
+                                    <b>
+                                      {solicitud.titulo
+                                        ? solicitud.titulo
+                                        : solicitud.inmueble?.titulo}
+                                    </b>
+                                  </div>
+                                  <div className="btns mt-3">
+                                    <span
+                                      onClick={() =>
+                                        aprobarSolicitud(
+                                          solicitud._id,
+                                          solicitud.inmueble.titulo,
+                                          solicitud.inmueble
+                                            ? solicitud.inmueble.imgs[0]
+                                            : "",
+                                          solicitud.usuario.correo
+                                        )
+                                      }
+                                      className={`${styles.aceptar} pointer`}
+                                    >
+                                      Aceptar
+                                    </span>{" "}
+                                    <span
+                                      onClick={() =>
+                                        rechazarSolicitud(
+                                          solicitud._id,
+                                          solicitud.inmueble.titulo,
+                                          solicitud.inmueble
+                                            ? solicitud.inmueble.imgs[0]
+                                            : "",
+                                          solicitud.usuario.correo
+                                        )
+                                      }
+                                      className={`${styles.rechazar} pointer`}
+                                    >
+                                      Rechazar
+                                    </span>
+                                  </div>
+                                  <div className={styles.hra}>
+                                    {hora(solicitud.createdAt)}
+                                  </div>
+                                </td>
+                              </>
+                            ) : solicitud.estado === "Aprobado" ? (
+                              <>
+                                <td valign="top">
+                                  <div className={styles.imgContainer}>
+                                    <img
+                                      src={solicitud.usuario.img}
+                                      alt={solicitud.usuario.nombre}
+                                      style={{
+                                        borderRadius: "50%",
+                                        width: "100%",
+                                      }}
+                                    />
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className={styles.notifiTextAprobado}>
+                                    {solicitud.usuario.nombre}{" "}
+                                    {solicitud.usuario.apellido} quiere que le
+                                    compartas{" "}
+                                    {solicitud.titulo
+                                      ? solicitud.titulo
+                                      : solicitud.inmueble?.titulo}
+                                  </div>
+                                  <div className="btns mt-3">
+                                    <span
+                                      className={`${styles.aceptar} pointer`}
+                                    >
+                                      Aceptado
+                                    </span>{" "}
+                                  </div>
+                                  <div className={styles.hra}>
+                                    {hora(solicitud.createdAt)}
+                                  </div>
+                                </td>
+                              </>
+                            ) : solicitud.estado === "Rechazado" ? (
+                              <>
+                                <td valign="top">
+                                  <div className={styles.imgContainer}>
+                                    <img
+                                      src={solicitud.usuario.img}
+                                      alt={solicitud.usuario.nombre}
+                                      style={{
+                                        borderRadius: "50%",
+                                        width: "100%",
+                                      }}
+                                    />
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className={styles.notifiTextAprobado}>
+                                    {solicitud.usuario.nombre}{" "}
+                                    {solicitud.usuario.apellido} quiere que le
+                                    compartas{" "}
+                                    {solicitud.titulo
+                                      ? solicitud.titulo
+                                      : solicitud.inmueble?.titulo}
+                                  </div>
+                                  <div className="btns mt-3">
+                                    <span
+                                      className={`${styles.rechazar} pointer`}
+                                    >
+                                      Rechazado
+                                    </span>{" "}
+                                  </div>
+                                  <div className={styles.hra}>
+                                    {hora(solicitud.createdAt)}
+                                  </div>
+                                </td>
+                              </>
+                            ) : (
+                              "Error. No debe llegar a esta parte"
+                            )}
+                          </tr>
+                        ))}
+                      </>
+                    )}
+                  </>
+                )}
               </table>
             </div>
           </div>
